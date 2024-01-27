7e13c0f348e8c9117b085613e2f99f340ff6de8a
root <root@varunlaptop>
2024-01-27 20:17:43 +0530

    modified kInversePairs.py file and solved the problem

--- python/kInversePairs(leetocde_629).py
+++ python/kInversePairs(leetocde_629).py
@@ -1,2 +1,17 @@
 class Solution:
   def kInversePairs(self, n: int, k: int) -> int:
+      MOD = 1000_000_007
+
+      prev = [0] * (k+1);
+      prev[0] = 1;
+
+      for N in range(1,n+1):
+          cur = [0]*(k+1)
+          total = 0
+          for K in range(0,k+1):
+              if K>N:
+                  total -= prev[K - N]
+              total += prev[K]
+              cur[K] = total%MOD
+          prev = cur
+      return prev[k] 
