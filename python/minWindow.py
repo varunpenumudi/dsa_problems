051c53505f87c190b9dc339f64037f243959bbd2
root <root@varunlaptop>
2024-02-04 14:19:59 +0530

    added boilerplate code for minWindow.py

---
+++ python/minWindow.py
@@ -0,0 +1,10 @@
+#leetcode problem 76 
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        ...
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert sol.minWindow("hello","ol") == "lo"
